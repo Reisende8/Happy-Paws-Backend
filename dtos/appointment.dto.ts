1931fe03e048dd90da0572115671afbc9f8b9606
Bianca Balasa <balasaandreabianca@gmail.com>
2023-06-04 15:35:12 +0300

    changed create appointment dto

--- dtos/appointment.dto.ts
+++ dtos/appointment.dto.ts
@@ -1,4 +1,10 @@
-export const createAppointmentDTO = (appointment, animal, medic) => ({
+export const createAppointmentDTO = (
+  appointment,
+  animal,
+  medic,
+  clinic,
+  user
+) => ({
   id: appointment.id,
   clientId: appointment.clientId,
   date: appointment.date,
@@ -9,10 +15,14 @@ export const createAppointmentDTO = (appointment, animal, medic) => ({
   animal: { id: animal.id, name: animal.name },
   medic: {
     id: medic.id,
-    clinicId: medic.clinicId,
     specializationId: medic.specializationId,
     firstName: medic.firstName,
     lastName: medic.lastName,
     estimatedPrice: medic.estimatedPrice,
+  },
+  clinic: {
+    name: clinic.name,
+    address: clinic.address,
+    phoneNumber: user.phoneNumber,
   },
 });
--- services/AppointmentsService.ts
+++ services/AppointmentsService.ts
@@ -2,6 +2,8 @@ import { createAppointmentDTO } from "../dtos/appointment.dto";
 import Animal from "../models/Animal";
 import Appointment from "../models/Appointment";
 import Client from "../models/Client";
+import Clinic from "../models/Clinic";
+import User from "../models/User";
 import Veterinarian from "../models/Veterinarian";
 import { createAppointmentInterface } from "./AppointmentsService.type";
 const { Op } = require("sequelize");
@@ -43,9 +45,19 @@ module.exports.createAppointment = async (
     where: { id: body.veterinarianId },
   });
 
+  const clinic = await Clinic.findOne({
+    where: { id: medic.clinicId },
+  });
+
+  const user = await User.findOne({
+    where: { id: userId },
+  });
+
   return createAppointmentDTO(
     createdAppointment.dataValues,
     animal.dataValues,
-    medic.dataValues
+    medic.dataValues,
+    clinic.dataValues,
+    user.dataValues
   );
 };
